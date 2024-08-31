dfb19fd3c8250aa7d75046a7550798c64ff7340a
Anastasia Zamkovskaya <trabemz@yandex.ru>
2024-08-31 23:11:20 +0300

    delete dublicate of name

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -4,7 +4,7 @@ import { ListProps } from './List.types';
 import { products } from '../../products';
 import { useLocalStorage } from 'react-use';
 
-const List: FC<ListProps> = ({ name, listByCategory, id: listId }) => {
+const List: FC<ListProps> = ({ listByCategory, id: listId }) => {
   const [checkedIds, setCheckedIds] = useLocalStorage<string[]>(
     `checked${listId}`,
     []
@@ -28,55 +28,45 @@ const List: FC<ListProps> = ({ name, listByCategory, id: listId }) => {
       setCheckedIds(newIds);
     }
   };
-  return (
-    <>
-      <h2>{name}</h2>
-      {Object.entries(listByCategory).map((item) => {
-        const [category, listOfCategory] = item as [
-          string,
-          Record<number, number>
-        ];
-        return (
-          <div key={category}>
-            <h2>{category}</h2>
-            <ListComponent
-              filterable={false}
-              items={Object.entries(listOfCategory) as [string, number][]}
-              renderItem={(item) => {
-                const [ingredientId, quantity] = item;
-                const ingredient = products.find(
-                  (product) => String(product.id) === String(ingredientId)
-                );
-                if (!ingredient) {
-                  return;
-                }
-                return (
-                  <span
-                    style={{
-                      textDecoration: checkedIds?.includes(ingredientId)
-                        ? 'line-through'
-                        : 'none',
-                    }}
-                  >
-                    {ingredient.name} - {Number(quantity)} {ingredient.unit}
-                  </span>
-                );
-              }}
-              onItemClick={onItemClick}
-              itemsHeight={1000}
-              filterItem={(filter) => (item) => {
-                const ingredient = products.find(
-                  (product) => String(product.id) === String(item[0])
-                );
-                return (
-                  ingredient !== undefined && ingredient.name.includes(filter)
-                );
-              }}
-            />
-          </div>
-        );
-      })}
-    </>
-  );
+
+  return Object.entries(listByCategory).map((item) => {
+    const [category, listOfCategory] = item as [string, Record<number, number>];
+    return (
+      <div key={category}>
+        <ListComponent
+          filterable={false}
+          items={Object.entries(listOfCategory) as [string, number][]}
+          renderItem={(item) => {
+            const [ingredientId, quantity] = item;
+            const ingredient = products.find(
+              (product) => String(product.id) === String(ingredientId)
+            );
+            if (!ingredient) {
+              return;
+            }
+            return (
+              <span
+                style={{
+                  textDecoration: checkedIds?.includes(ingredientId)
+                    ? 'line-through'
+                    : 'none',
+                }}
+              >
+                {ingredient.name} - {Number(quantity)} {ingredient.unit}
+              </span>
+            );
+          }}
+          onItemClick={onItemClick}
+          itemsHeight={1000}
+          filterItem={(filter) => (item) => {
+            const ingredient = products.find(
+              (product) => String(product.id) === String(item[0])
+            );
+            return ingredient !== undefined && ingredient.name.includes(filter);
+          }}
+        />
+      </div>
+    );
+  });
 };
 export default List;
